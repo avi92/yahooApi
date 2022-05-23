d941fa2206a8e1cd7799683cddd5030219d9f51d
avi92 <axelvirnich@googlemail.com>
2022-05-23 15:27:47 +0200

    fix period parameter

--- src/main/kotlin/de/virnich/yahooApi/model/request/Period.kt
+++ src/main/kotlin/de/virnich/yahooApi/model/request/Period.kt
@@ -9,9 +9,9 @@ package de.virnich.yahooApi.model.request
 enum class Period(val parameter: String) {
     ONE_DAY("1d"),
     FIVE_DAYS("5d"),
-    ONE_MONTH("1m"),
-    THREE_MONTHS("3m"),
-    SIX_MONTHS("6m"),
+    ONE_MONTH("1mo"),
+    THREE_MONTHS("3mo"),
+    SIX_MONTHS("6mo"),
     ONE_YEAR("1y"),
     TWO_YEARS("2y"),
     FIVE_YEARS("5y"),
